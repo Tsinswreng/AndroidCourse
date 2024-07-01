3fd2be541295b6b7c3cf80a3b6730f3520c090c8
Tsinswreng <2290019838@qq.com>
2024-07-01 08:47:35 +0800

    備 㕥主支合己

--- Exp2/app/src/main/java/com/tsinswreng/exp2/MainActivity.kt
+++ Exp2/app/src/main/java/com/tsinswreng/exp2/MainActivity.kt
@@ -9,27 +9,7 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_login)
-		// 初始化 Position 对象
-		position = Position(this)
-		// 请求位置信息
-		position.getLocation { latitude, longitude ->
-			Toast.makeText(this, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_LONG).show()
-			println("zzzz-")
-			println(latitude)
-			println(longitude)
-		}
-		
+
 	}
 	
-	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-		
-		// 将权限结果传递给 Position 对象，并重新尝试获取位置信息
-		position.onRequestPermissionsResult(requestCode, grantResults) { latitude, longitude ->
-			Toast.makeText(this, "Latitude: $latitude, Longitude: $longitude", Toast.LENGTH_LONG).show()
-		}
-	}
-	
-
-
 }
